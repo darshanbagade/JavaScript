a94dc2d72c5369003100f24e31c271515e07f2a8
Darshan Bagade <161910663+darshanbagade@users.noreply.github.com>
2024-11-14 19:14:32 +0000

    01_variables

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,17 @@
+
+const accountId = 23070912; 
+let accountEmail = "darshan@gmail.com";
+var accountPassword = "abcdefghijklmnopqrstuvwxyz";
+accountCity = "Mumbai"; 
+
+// accountId ="123212" //re-assigning to 'const' is not allowed
+
+/*
+Prefer not to use var
+beacause of the issue block scope and funtion scope
+*/
+
+
+//display of all vaules in table 
+console.table([accountId ,accountEmail, accountPassword, accountCity]);
+
