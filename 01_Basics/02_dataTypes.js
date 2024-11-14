53aa009268d0c3c9eea8f8d91ebd541d05765249
Darshan Bagade <161910663+darshanbagade@users.noreply.github.com>
2024-11-14 19:14:55 +0000

    02_datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3 + 4); // we are using node js, not browser
+
+//coding readability should be high
+console.log(3+3);
+console.log("Darshan");
+
+
+
+// number =>  2 to power 53
+// bigint =>  a very big number
+// string => ""
+// boolean => true/flase
+// null => standalone value
+// undefined => type isn't defined
+// symbol => unique (will used in react)
+// object
+
+console.log(typeof null) // typeof null is object
