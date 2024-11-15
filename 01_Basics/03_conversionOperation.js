a8a38a7e96a8be63c85741cc978aa3789a629869
Darshan Bagade <161910663+darshanbagade@users.noreply.github.com>
2024-11-15 17:28:56 +0000

    03_conversionOperation

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -20,7 +20,7 @@
 
 
 //conversion to boolean
-// let IsLoggedIn = undefined;
+// let IsLoggedIn = "";
 // console.log(typeof IsLoggedIn);
 
 // let booleanIsLoggedIn = Boolean(IsLoggedIn);
@@ -33,20 +33,61 @@
 // 0 => flase
 // null => false
 //undefined => false
-
+// "" => flase
 
 
 //Converison to string
 
-let someString= true ;
-console.log(typeof someString);
+// let someString= true ;
+// console.log(typeof someString);
 
-let conversionToString = String(someString);
-console.log(typeof conversionToString);
-console.log(conversionToString);
+// let conversionToString = String(someString);
+// console.log(typeof conversionToString);
+// console.log(conversionToString);
 
 // 33 => "33" string
 // null => null
 // true => "true"
 
+//----------------------------Operations-----------------------------//
 
+// let value = 5;
+// let negValue = -value;
+// console.log(negValue); 
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3); // 2 to the power 3
+// console.log(2/2);
+// console.log(2%2);
+
+
+
+// let str1 ="Hello";
+// let str2 =" darshan"
+// let str3 = str1 + str2;
+// console.log(str3);
+
+
+// console.log("1" + 2 + 2) //122
+// console.log(1 + 2 + "2") //32
+
+// console.log(+true); //1
+// console.log(+""); //0
+
+
+
+// increment and decrement
+//link -  https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+// let x = 3;
+// const y = x++;
+
+// console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+// let a = 3;
+// const b = ++a;
+
+// console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
