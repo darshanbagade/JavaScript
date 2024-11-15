2ccf8aa06b1f2fa62af1721830e2528158dd701f
Darshan Bagade <161910663+darshanbagade@users.noreply.github.com>
2024-11-15 17:00:18 +0000

    03_conversionOperations (conversion of datatypes)

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,52 @@
+
+//while conversion to any datatype write first letter Capital
+
+// let score = 33;
+
+// console.log(typeof score)
+// console.log(typeof(score));
+
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log( valueInNumber);
+
+// conversion to number
+// "33" => 33
+// "33abc" => NaN
+// "true" => 1
+// "false" => 0
+
+
+
+//conversion to boolean
+// let IsLoggedIn = undefined;
+// console.log(typeof IsLoggedIn);
+
+// let booleanIsLoggedIn = Boolean(IsLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+//conversion to boolean
+//"darshan" => true
+// 1 => true
+// 0 => flase
+// null => false
+//undefined => false
+
+
+
+//Converison to string
+
+let someString= true ;
+console.log(typeof someString);
+
+let conversionToString = String(someString);
+console.log(typeof conversionToString);
+console.log(conversionToString);
+
+// 33 => "33" string
+// null => null
+// true => "true"
+
+
