c3e27896d7d2052a9081478f44a0b4586f95f722
Darshan Bagade <161910663+darshanbagade@users.noreply.github.com>
2024-11-16 17:17:36 +0000

    Stack and Heap

--- 01_Basics/05_datatypesSummary.js
+++ 01_Basics/05_datatypesSummary.js
@@ -35,3 +35,32 @@
 // console.log(typeof null); //object
 // console.log(typeof myFunction) // function
 // console.log(typeof myObj) //object
+
+
+
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++\\
+//stack(premitives are stored)
+//heap( non premitives are stored)
+
+
+//stack
+// const name1 = "darshan";
+// let name2 = name1; //copy of name1 is assigned , not reference
+// name2= "aryan";
+// console.log(name1);
+// console.log(name2);
+
+//heap
+let obj1 = {
+    email:"darshan@gmail.com",
+    age:20
+}
+let obj2 = obj1; //actual reference of the obj1 is assigned to the obj2
+
+obj2.email= "darshanbagade@yahoo.com";
+//email of obj1 is alos changed
+
+console.log(obj1);
+console.log(obj2);
