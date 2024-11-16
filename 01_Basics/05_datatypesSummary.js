4719dffcd65e7413075fbbb4f647da291d67c550
Darshan Bagade <161910663+darshanbagade@users.noreply.github.com>
2024-11-16 16:45:28 +0000

    05_datatypesSummary

---
+++ 01_Basics/05_datatypesSummary.js
@@ -0,0 +1,37 @@
+//Datatypes are divided into two types on the basis of :  How data is stored and How to access the data
+
+//dynamically typed
+// JavaScript is a weakly typed programming language because you do not have to specify the variable type in advance.
+
+//Premitives :
+// 7 categories : String , Boolean, null, undefined, Number, symbol, BigInt
+
+// const id =Symbol("123");
+// const newId =Symbol("123");
+// console.log(id === newId)
+// console.log(typeof id);//symbol
+
+// const bigInt = 13243345546544535654n;
+
+
+
+
+//Reference(Non-Premitives) :
+//Array, Objects, Functions
+
+// const arr = ["darshan" , "aryan" , "nutan"];
+
+// let myObj= {
+//     name : "darshan",
+//     age : "20"
+// }
+
+// const myFunction = function(){
+//     console.log("Hello Darshan");
+// }
+// myFunction();
+
+//to display the datatype of variables
+// console.log(typeof null); //object
+// console.log(typeof myFunction) // function
+// console.log(typeof myObj) //object
