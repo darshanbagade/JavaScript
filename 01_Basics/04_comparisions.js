62e023361871cdc742db2e49941f30780fc14767
Darshan Bagade <161910663+darshanbagade@users.noreply.github.com>
2024-11-16 16:44:57 +0000

    04_comparisions

---
+++ 01_Basics/04_comparisions.js
@@ -0,0 +1,16 @@
+// console.log(2>1);
+// console.log( 3 == 4);
+// console.log( "4" == 4);
+// console.log( "04" == 4);
+
+//this is not good practice of comparisions
+// console.log(null == 0)
+// console.log(null > 0)
+// console.log(null < 0)
+// console.log(null <= 0)
+
+//equality  == ,and comparision operators(< , > , >= , <=) both have different meaning
+
+
+//console.log("2" == 2);  // only checks the value
+//console.log("2" === 2); //checks value + datatype
